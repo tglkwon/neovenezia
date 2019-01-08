77c98d3cb875c8db9ce18c4a96825909e8476699
tglkwon <tglkwon@gmail.com>
2019-01-09 01:57:27 +0900

    20190109 tenhou AI max KaWa

---
+++ C++/C++Test/C++Test/main.cpp
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+int main(){
+	std::string echoo = "Hello World!";
+	printf("%s", echoo.c_str());
+
+	std::cin.get();
+	return 0;
+}
+
+
+/*
+#include <iostream>
+
+int main ()
+{
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
+*/
