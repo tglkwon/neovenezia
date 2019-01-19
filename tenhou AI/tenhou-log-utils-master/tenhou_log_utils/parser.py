4bf557c64e4f3bdfe9f39857e8cf96d26c50cd98
tglkwon <43848912+tglkwon@users.noreply.github.com>
2019-01-19 20:33:03 +0900

    20190118 tenhou AI Kan

--- tenhou AI/tenhou-log-utils-master/tenhou_log_utils/parser.py
+++ tenhou AI/tenhou-log-utils-master/tenhou_log_utils/parser.py
@@ -239,7 +239,7 @@ def _parse_kan(meld):
         hai0, h[2] = h[2], hai0
     if kui == 2:
         hai0, h[0] = h[0], hai0
-    return ([hai0] + h) if kui else h[:2]
+    return ([hai0] + h) if kui else h
 
 
 def _parse_call(attrib):
